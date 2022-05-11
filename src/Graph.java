0b96a33d2a6aa290b49ba45e38787daa7dff1771
Dominika <wole@nie.dzieki>
2022-05-12 01:03:01 +0200

    Zmiana konstruktora

---
+++ src/Graph.java
@@ -0,0 +1,134 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Graph {
+    private Node[] nodes;
+    private int nbOfGraphs;
+    private int length;
+    private int width;
+
+    /*Konstruktor generujący*/
+    public Graph(int width, int length, double upper, double lower) {
+        generateGraph(width, length, upper, lower);
+        this.nbOfGraphs = 1;
+    }
+
+    /*Konstruktor czytający*/
+    public Graph(String inFile) {
+        ;
+    }
+
+    public void printGraphToFile(String outFileName) {
+        try {
+            FileWriter out = new FileWriter(outFileName);
+            out.append(this.width + " " + this.length + "\n");
+            for (int i = 0; i < this.nodes.length; i++) {
+                Node currentNode = nodes[i];
+                for (int j = 0; j < currentNode.getWays(); j++) {
+                    out.append("\t" + currentNode.getConnAtIndex(j).getId() + ": " + currentNode.getEdgeAtIndex(j));
+                }
+                out.append("\n");
+            }
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkIntegrity() {
+        Node current = nodes[0];
+        LinkedList<Node> working = new LinkedList<>();
+        LinkedList<Node> checked = new LinkedList<>();
+        working.add(current);
+
+        while (!working.isEmpty()) {
+            for (Node node : current.getConn()) {
+                if (!working.contains(node) && !checked.contains(node))
+                    working.add(node);
+            }
+            checked.add(working.poll());
+            current = working.peek();
+        }
+
+        if (checked.size() == this.nodes.length)
+            return true;
+        return false;
+
+    }
+
+    private void generateGraph(int width, int length, double upper, double lower) {
+        Node[] genNodes = new Node[width * length];
+        for (int i = 0; i < length * width; i++) {
+            int currentWays = determineWays(i, width, length);
+            genNodes[i] = new Node(i, currentWays, new Node[currentWays], new double[currentWays]);
+        }
+        this.nodes = genNodes;
+        this.nbOfGraphs = 1;
+        this.width = width;
+        this.length = length;
+        if (upper < lower) {
+            double temp = upper;
+            upper = lower;
+            lower = temp;
+        }
+        for (int i = 0; i < genNodes.length; i++) {
+            fillRandomConnections(i, lower, upper);
+        }
+    }
+
+    private int determineWays(int id, int width, int length) {
+        int ways;
+        if (width == 1 && length == 1)
+            ways = 0;
+        else if (id == 0 || id == width - 1 || id == width * length - 1 || id == width * (length - 1)) {
+            if (width == 1 || length == 1)
+                ways = 1;
+            else
+                ways = 2;
+        } else if ((id > 0 && id < width - 1) || (id > (width - 1) * length && id < width * length - 1) || id % width == 0 || id % width == width - 1) {
+            if (width == 1 || length == 1)
+                ways = 2;
+            else
+                ways = 3;
+        } else
+            ways = 4;
+
+        return ways;
+    }
+
+    private void fillRandomConnections(int current, double lower, double upper) {
+        Random random = new Random();
+        Node currentNode = this.nodes[current];
+        int right = currentNode.getId() + 1;
+        int left = currentNode.getId() - 1;
+        int up = currentNode.getId() - this.width;
+        int down = currentNode.getId() + this.width;
+        int i = 0;
+
+        if (right % width != 0 && right < nodes.length && width != 1) {
+            currentNode.setConnAtIndex(i, nodes[right]);
+            currentNode.setEdgeAtIndex(i, lower + (upper - lower) * random.nextDouble());
+            i++;
+        }
+        if (down < nodes.length) {
+            currentNode.setConnAtIndex(i, nodes[down]);
+            currentNode.setEdgeAtIndex(i, lower + (upper - lower) * random.nextDouble());
+            i++;
+        }
+        if (up >= 0) {
+            currentNode.setConnAtIndex(i, nodes[up]);
+            int index = nodes[up].getIndexOfConnection(currentNode.getId());
+            if (index >= 0)
+                currentNode.setEdgeAtIndex(i, nodes[up].getEdgeAtIndex(index));
+            i++;
+        }
+        if (left % width != width - 1 && left >= 0 && width != 1) {
+            currentNode.setConnAtIndex(i, nodes[left]);
+            int index = nodes[left].getIndexOfConnection(currentNode.getId());
+            if (index >= 0)
+                currentNode.setEdgeAtIndex(i, nodes[left].getEdgeAtIndex(index));
+        }
+    }
+}
