5c6fe91342c6f5a338962f91df61c54ab114cb35
Dominika Gołębiewska <01169559@pw.edu.pl>
2022-06-15 17:09:05 +0200

    Jakies bledy byly

--- src/test/java/BFSTest.java
+++ src/test/java/BFSTest.java
@@ -1,6 +1,3 @@
-package Test;
-
-import Szukajava.Graph;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,13 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BFSTest {
     @Test
     public void connectedGraphTest(){
-        Graph graph = new Graph("src/test/java/Test.resources/BfsTestConnected.txt");
+        Graph graph = new Graph("src/test/java/resources/BfsTestConnected.txt");
         assertEquals(true, graph.checkIntegrity());
     }
 
     @Test
     public void unconnectedGraphTest(){
-        Graph graph = new Graph("src/test/java/Test.resources/BfsTestUnconnected.txt");
+        Graph graph = new Graph("src/test/java/resources/BfsTestUnconnected.txt");
         assertEquals(false, graph.checkIntegrity());
     }
 }
